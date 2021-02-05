bc909e1ab9cdf64465ba6af0622919a8f5cc7d6b
Jesse Marques <veteranojunior@hotmail.com>
2021-02-04 21:43:51 -0300

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="input-group">
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Dollar amount (with dot and two decimal places)"
+          v-model="country"
+        />
+        <button type="button" @click="search" class="btn btn-primary btn-lg">
+          Search
+        </button>
+      </div>
+      <h2 class="text-muted" v-if="erro" style="color: red">
+        No country found, please write the full name
+      </h2>
+
+      <!-- CARD -->
+      <div v-if="info" class="card m-2" style="width: 400px">
+        <div class="card-body row">
+          <img class="col" :src="flag" style="width: 50%; border-radius: 50%" />
+          <h2 class="col text-center mt-5">{{ info }}</h2>
+        </div>
+      </div>
+      <!-- <pre>{{ flag }}</pre> -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      info: null,
+      country: "",
+      flag: "",
+      erro: false,
+    };
+  },
+  created() {},
+  methods: {
+    search() {
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${this.country}?fullText=true`)
+        .then((response) => {
+          console.log(response.data),
+            (this.info = response.data[0].name),
+            (this.flag = response.data[0].flag);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.info = null;
+          this.erro = true;
+          setTimeout(() => {
+            this.erro = false;
+          }, 2000);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,7 +69,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
 }
