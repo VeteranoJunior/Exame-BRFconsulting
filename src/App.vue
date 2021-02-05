b447e096d447ee2311ee57156c22dde385e21128
Jesse Marques <veteranojunior@hotmail.com>
2021-02-04 22:22:36 -0300

    list countrys

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,14 @@
       <h2 class="text-muted" v-if="erro" style="color: red">
         No country found, please write the full name
       </h2>
+      <div v-if="start">
+        <div v-for="(item, idx) in start.slice(0, 10)" :key="idx" class="card m-2" style="width: 400px">
+          <div class="card-body row">
+            <img class="col" :src="item.flag" style="width: 50%; border-radius: 50%" />
+            <h2 class="col text-center mt-5">{{ item.name }}</h2>
+          </div>
+        </div>
+      </div>
 
       <!-- CARD -->
       <div v-if="info" class="card m-2" style="width: 400px">
@@ -23,7 +31,7 @@
           <h2 class="col text-center mt-5">{{ info }}</h2>
         </div>
       </div>
-      <!-- <pre>{{ flag }}</pre> -->
+      <!-- <pre>{{ start.slice(0, 20) }}</pre> -->
     </div>
   </div>
 </template>
@@ -39,15 +47,22 @@ export default {
       country: "",
       flag: "",
       erro: false,
+      start: null,
     };
   },
-  created() {},
+  created() {
+      axios
+        .get("https://restcountries.eu/rest/v2/all")
+        .then((response) => {
+          this.start = response.data
+        })
+  },
   methods: {
     search() {
+      this.start = null
       axios
         .get(`https://restcountries.eu/rest/v2/name/${this.country}?fullText=true`)
         .then((response) => {
-          console.log(response.data),
             (this.info = response.data[0].name),
             (this.flag = response.data[0].flag);
         })
